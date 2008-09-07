4ae04de37bcc635c8a8efa02fa60e90e2f89e131
alfredr <none@none>
2008-09-07 13:05:45 -0400

    [svn r2] clearing repo
    --HG--
    branch : trunk

--- libs/pygame-1.8.1release/test/run_tests__tests/all_ok/zero_tests_test.py
+++
@@ -1,8 +0,0 @@
-import test_utils
-import test.unittest as unittest
-
-class KeyModuleTest(unittest.TestCase):
-    pass
-
-if __name__ == '__main__':
-    unittest.main()
--- libs/pymunk-0.8/tools/generate_docs.py
+++
@@ -1,9 +0,0 @@
-import epydoc.cli
-import sys
-
-def main():
-    sys.argv[1:] = ["--config=epydoc.config"]   
-    epydoc.cli.cli()
-
-if __name__ == "__main__":
-    sys.exit(main())
