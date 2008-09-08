7ddd5bcddb4319859f52560d30ab6c6771998e3c
alfredr <none@none>
2008-09-08 15:57:44 -0400

    [svn r9] making changes toward extensible design w/ entity,manager classes for interacting objects
    --HG--
    branch : trunk

--- gamelib/main.py
+++ gamelib/main.py
@@ -10,51 +10,131 @@ import data, pymunk, pygame
 from pymunk.vec2d import Vec2d
 from pygame.locals import *
 
+class WorldEntity(object):
+    _shape = None
+    _body = None
+    _pinjoints = []
+
+    def get_shape(self):
+        return self._shape
+
+    def get_body(self):
+        return self._body
+
+    def get_vertices(self):
+        return self._shape.get_points()
+
+    def on_collision(self, other_entity):
+#        print "%s hit %s" % (self, other_entity)
+        pass
+
+class WorldEntityManager(object):
+    _entities = []
+    _group_count = 0
+
+    def __init__(self, space):
+        self._space = space
+
+    def alloc_collision_group(self):
+        self._group_count += 1
+        return self._group_count
+
+    def create_worldentity(self, world_pos, vertices, mass, dynamic=True, friction = 0.25, collision_group=0):
+        pymunk_verts = map(Vec2d, vertices)
+        
+        inertia = pymunk.moment_for_poly(mass, pymunk_verts, (0,0))
+        body = pymunk.Body(mass, inertia)
+        body.position = world_pos
+  
+        shape = pymunk.Poly(body, pymunk_verts, (0,0) )
+        shape.friction = friction
+        shape.group = collision_group
+        
+        ent = WorldEntity()
+        ent._shape = shape
+        ent._body = body
+
+        self._entities.append((shape, ent))
+
+        self._space.add(shape)
+        if dynamic:
+            self._space.add(body)
+
+        return ent
+
+    def pinjoin_worldentities(self, ent_a, ent_b, pos_a, pos_b):
+        joint = pymunk.PinJoint(ent_a.get_body(), ent_b.get_body(), pos_a, pos_b)
+        ent_a._pinjoints.append(joint)
+        ent_b._pinjoints.append(joint)
+        self._space.add(joint)
+
+    def get_entity_by_shape(self, shape):
+        for ent_shape, ent in self._entities:
+            if ent_shape == shape:
+                return ent
+
+        return None
+
+    def on_collision(self, shapeA, shapeB, contacts, normal_coef, data):
+        ent_a = self.get_entity_by_shape(shapeA)
+        ent_b = self.get_entity_by_shape(shapeB)
+
+        if ent_a and ent_b:
+            ent_a.on_collision(ent_b)
+            ent_b.on_collision(ent_a)
+        
+        return True
+
+    def get_entities(self):
+        return [ent for (shape, ent) in self._entities]
+
+
 PIX_PER_M = 2 #pixels per meter
 
-chain_link_len = 10
-chain_link_poly = map(Vec2d, [(0,0), (0,5), (chain_link_len, 5), (chain_link_len, 0)])
-print chain_link_poly
+SCREENSIZE = (800, 600)
 
-def make_chain(space, length):
+chain_link_len = 10
+chain_link_poly = [(0,0), (0,5), (chain_link_len, 5), (chain_link_len, 0)]
+
+def make_chain(we_manager, length, allow_self_intersection = False):
+    
+    cgrp = 0
+    if allow_self_intersection:
+        cgrp = we_manager.alloc_collision_group()
+
     links = []
     for i in range(length):
         mass = 5
-        inertia = pymunk.moment_for_poly(mass, chain_link_poly, (0,0))
-        body = pymunk.Body(mass, inertia)
-        shape = pymunk.Poly(body, chain_link_poly, (0,0))
-        body.position = (i*(chain_link_len), 400)
-        links.append((body, shape))
-        space.add(body, shape)
-
+        links.append(we_manager.create_worldentity((i*(chain_link_len+2), 400), chain_link_poly, mass, collision_group=cgrp))
         if i > 0:
-            chain_joint = pymunk.PinJoint(body, links[i-1][0], (0,0), (chain_link_len, 0))
-            space.add(chain_joint)
+            we_manager.pinjoin_worldentities(links[i], links[i-1], (0,2.5), (chain_link_len, 2.5))
     
     return links
-        
+
+
+def to_scr(v):
+    return (v[0], (SCREENSIZE[1]/2)-v[1])
 
 def main():
     
     #init pygame
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode(SCREENSIZE)
     pygame.display.set_caption('### OSUGCC PYWEEK PROTOTYPE')
 
     #init pymunk
     pymunk.init_pymunk()
     space = pymunk.Space()
     space.gravity = (0.0, -9.8 * PIX_PER_M)
+    space.damping = 0.98
     space.resize_static_hash()
     space.resize_active_hash()
 
-
-    ledge_body = pymunk.Body(pymunk.inf, pymunk.inf)
-    ledge_body.position = Vec2d(0, 100)
-    ledge_line = pymunk.Segment(ledge_body, Vec2d(-100,0), Vec2d(100, 0), 5.0)
-    space.add(ledge_line)
-
-    chain = make_chain(space, 15)
+    we_manager = WorldEntityManager(space)
+    we_manager.create_worldentity((0,-20), [(0,0),(0, 20), (700, 20), (700, 0)], pymunk.inf, dynamic=False)
+    we_manager.create_worldentity((0,200), [(0,0),(0, 20), (300, 20), (300, 0)], pymunk.inf, dynamic=False)
+    
+    space.set_default_collisionpair_func(we_manager.on_collision)
 
     #pygame event loop
     is_running = True
@@ -62,12 +142,9 @@ def main():
     while is_running:
         screen.fill((0,0,0))
 
-        pygame.draw.line(screen, (255,255,255), (-100,100), (100,100), 1)
-
-        for link in chain:
-            px, py = (int(link[0].position.x), int(link[0].position.y))
-            pygame.draw.polygon(screen, (255,255,255), link[1].get_points(), 1)
-#            pygame.draw.rect(screen, (255,255,255), (px-chain_link_len/2.0, py, chain_link_len, 5), 1)
+        for entity in we_manager.get_entities():
+            points = map(to_scr, entity.get_vertices())
+            pygame.draw.polygon(screen, (255,255,255), points, 1)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -78,6 +155,10 @@ def main():
                     is_running = False
                 elif event.key == K_s:
                     space.step(1/50.0)
+                elif event.key == K_v:
+                    make_chain(we_manager, 30, True)
+                elif event.key == K_c:
+                    make_chain(we_manager, 30, False)
                 elif event.key == K_SPACE:
                     with_physics = not with_physics
 
