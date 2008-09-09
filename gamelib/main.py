b5963360ea51436b4de31194255bc83d93203201
alfredr <none@none>
2008-09-09 12:27:36 -0400

    [svn r12] basic player pickup interaction
    --HG--
    branch : trunk

--- gamelib/main.py
+++ gamelib/main.py
@@ -11,8 +11,8 @@ from pymunk.vec2d import Vec2d
 from pygame.locals import *
 
 class WorldEntity(object):
-    _pinjoints = []
-    _slidejoints = []
+    _world_entity_manager = None
+    _is_dynamic = True
 
     def __init__(self, world_pos, vertices, mass, friction=0.15, moment = None):
         pymunk_verts = map(Vec2d, vertices)
@@ -26,6 +26,27 @@ class WorldEntity(object):
         self._shape = pymunk.Poly(self._body, pymunk_verts, (0,0) )
         self._shape.friction = friction
 
+    def is_world_bound(self):
+        return self._world_entity_manager != None
+
+    def is_dynamic(self):
+        return self._is_dynamic
+
+    def on_bind_world(self, world_entity_manager):
+        self._world_entity_manager = world_entity_manager
+
+    def pin_join(self, entity, self_pos, entity_pos):
+        if self.is_world_bound():
+            return self._world_entity_manager.pin_join_entities(self, entity, self_pos, entity_pos)
+
+        return False
+
+    def unjoin(self, joint_id):
+        if self.is_world_bound():
+            return self._world_entity_manager.unjoin(joint_id)
+
+        return False
+
     def get_shape(self):
         return self._shape
 
@@ -35,7 +56,7 @@ class WorldEntity(object):
     def get_vertices(self):
         return self._shape.get_points()
 
-    def on_collision(self, other_entity):
+    def on_collision(self, other_entity, contacts, normal_coef, data):
 #        print "%s hit %s" % (self, other_entity)
         pass
 
@@ -45,6 +66,16 @@ class WorldEntity(object):
 class WorldEntityManager(object):
     _entities = {} 
     _group_count = 0
+    _joint_count = 0
+    _joints = {}
+
+    PINJOINT = 0
+    
+    JINF_TYPE = 0
+    JINF_JOINT = 1
+    JINF_ENTA = 2
+    JINF_ENTB = 3
+    JINF_DATA = 4
 
     def __init__(self, space):
         self._space = space
@@ -53,10 +84,30 @@ class WorldEntityManager(object):
         self._group_count += 1
         return self._group_count
 
+    def _alloc_joint_id(self):
+        self._joint_count += 1
+        return self._joint_count
+
+    def _register_joint(self, joint_type, joint, ent_a, ent_b, data):
+        joint_id = self._alloc_joint_id()
+        self._joints[joint_id] = (joint_type, joint, ent_a, ent_b, data)
+        return joint_id
+    
+    def unjoin(self, joint_id):
+        if not joint_id in self._joints:
+            return False
+
+        joint_info = self._joints[joint_id]
+        self._space.remove(joint_info[WorldEntityManager.JINF_JOINT])
+        
+        return True
+
     def add_entity(self, entity, dynamic=True, collision_group=0):
         shape = entity.get_shape()
         shape.group = collision_group
-        
+
+        entity.on_bind_world(self)
+        entity._is_dynamic = dynamic
         self._entities[shape] = entity
 
         if dynamic:
@@ -64,25 +115,18 @@ class WorldEntityManager(object):
         else:
             self._space.add_static(shape)
 
-    def pinjoin_entities(self, ent_a, ent_b, pos_a, pos_b):
+    def pin_join_entities(self, ent_a, ent_b, pos_a, pos_b):
         joint = pymunk.PinJoint(ent_a.get_body(), ent_b.get_body(), pos_a, pos_b)
-        ent_a._pinjoints.append(joint)
-        ent_b._pinjoints.append(joint)
         self._space.add(joint)
-
-    def slidejoin_entities(self, ent_a, ent_b, pos_a, pos_b, min, max):
-        joint = pymunk.SlideJoint(ent_a.get_body(), ent_b.get_body(), pos_a, pos_b, min, max)
-        ent_a._slidejoints.append(joint)
-        ent_b._slidejoints.append(joint)
-        self._space.add(joint)
+        return self._register_joint(WorldEntityManager.PINJOINT, joint, ent_a, ent_b, (pos_a, pos_b))
 
     def on_collision(self, shapeA, shapeB, contacts, normal_coef, data):
         ent_a = self._entities[shapeA]
         ent_b = self._entities[shapeB]
 
         if ent_a and ent_b:
-            ent_a.on_collision(ent_b)
-            ent_b.on_collision(ent_a)
+            ent_a.on_collision(ent_b, contacts, normal_coef, data)
+            ent_b.on_collision(ent_a, contacts, normal_coef, data)
         
         return True
 
@@ -99,9 +143,14 @@ class Player(WorldEntity):
     LEFT = -1
     RIGHT = 1
 
-    def __init__(self, power = 100000):
-        WorldEntity.__init__(self, (400,400), [(-5,-10),(-5,10),(5,10),(5,-10)], 25, moment=pymunk.inf)
+    _hold_joint = None
+    _hand_location = (-10, 0)
+    _try_grab = False
+
+    def __init__(self, power = 3000):
+        WorldEntity.__init__(self, (400,400), [(-10,-20),(-10,20),(10,20),(10,-20)], 25, moment=pymunk.inf)
         self._power = power
+        self._direction = Player.STOP
         self.stop()
 
     def left(self):
@@ -116,10 +165,32 @@ class Player(WorldEntity):
     def jump(self):
         self.get_body().apply_impulse((0,5000), (0,0))
 
+    def begin_grabbing(self):
+        self._try_grab = True
+
+    def end_grabbing(self):
+        self._try_grab = False
+
+    def drop(self):
+        if self._hold_joint:
+            self.unjoin(self._hold_joint)
+            self._hold_joint = None
+
+    def hold(self, entity, entity_pos):
+        self.drop()
+        joint_id = self.pin_join(entity, self._hand_location, entity_pos)
+        if joint_id:
+            self._hold_joint = joint_id
+
+    def on_collision(self, entity, contacts, normal_coef, data):
+        if self._try_grab and entity.is_dynamic():
+            self.hold(entity, entity.get_body().world_to_local(contacts[0].position))
+            self.end_grabbing()
+
     def tick(self, dt):
         body = self.get_body()
         if abs(body.velocity[0]) < 200:
-            self.get_body().apply_impulse((self._direction*self._power*dt, 0),(0,0))
+            self.get_body().apply_impulse((self._direction*self._power, 0),(0,0))
 
 PIX_PER_M = 40 #pixels per meter
 
@@ -142,7 +213,7 @@ def make_chain(we_manager, length, allow_self_intersection = False):
         we_manager.add_entity(entity, collision_group=cgrp)
 
         if i > 0:
-            we_manager.pinjoin_entities(links[i], links[i-1], (0,2.5), (chain_link_len, 2.5))
+            we_manager.pin_join_entities(links[i], links[i-1], (0,2.5), (chain_link_len, 2.5))
     
     return links
 
@@ -182,7 +253,7 @@ def main():
     unused_time = 0
     step_size = 0.015
 
-    keydown_map = {K_w: False, K_s: False, K_d: False, K_a: False}
+    keydown_map = {K_w: False, K_d: False, K_a: False}
     #pygame event loop
     is_running = True
     while is_running:
@@ -212,10 +283,16 @@ def main():
                     make_chain(we_manager, 15, False)
                 elif event.key == K_SPACE:
                     player.jump()
+                elif event.key == K_s:
+                    player.begin_grabbing()
+                elif event.key == K_q:
+                    player.drop()
 
             elif event.type == KEYUP:
                 if event.key in keydown_map:
                     keydown_map[event.key] = False
+                elif event.key == K_s:
+                    player.end_grabbing()
 
         #perframe actions
         if keydown_map[K_a]:
@@ -237,8 +314,6 @@ def main():
         #render fps
         text_surf = font.render("fps: %i" % clock.get_fps(), 1, (255,0,0))
         screen.blit(text_surf, (5, 5))
-
-#            player.get_body().apply_impulse((10, 0), (0,0))
 
         pygame.display.flip()
 
